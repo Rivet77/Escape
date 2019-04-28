21279bc1e789bd27349262bb34c775b08d227911
Riley Tucker <39104746+RileyT@users.noreply.github.com>
2019-04-28 01:32:01 -0400

    Refactor Areas class
    Areas can now be called to make an Areas object.
    Added methods.

--- SemesterProject/src/semesterproject/Areas.java
+++ SemesterProject/src/semesterproject/Areas.java
@@ -7,30 +7,35 @@ package semesterproject;
 import javafx.scene.image.Image;
 
 public class Areas {
-    // global variables
+	// global variables
     private static int currentArea;
     private static int lastArea;
-    //first time to area boolean
-    static boolean beenTo1;
-    static boolean beenTo2;
-    static boolean beenTo3;
-    static boolean beenTo4;
-    static boolean beenTo5;
-    static boolean beenTo6;
-    static boolean beenTo7;
-    static boolean beenTo8;
-    static boolean beenTo9;
-    static boolean beenTo10;
-    static boolean beenTo11;
-    static boolean beenTo12;
-    static boolean beenTo13;
-    static boolean beenTo14;
-    static boolean beenTo15;
-    static boolean beenTo16;
-    static boolean beenTo17;
-    // room images
-    static Image room0 = new Image("semesterProject/mapRooms/room0.jpg");
-    static Image room4 = new Image("semesterProject/mapRooms/floor1.jpg");
+    private static String itemName = "";
+    private static boolean[] beenTo = new boolean[26];
+    private static boolean[] hasItem = new boolean[26];
+    private static boolean[] gottenItem = new boolean[26];
+    private static Areas[] roomsArray = new Areas[26];
+    private static String roomImagePath = "semesterProject/mapRooms/room";
+    private static Image roomPic = new Image(roomImagePath + currentArea);
+    
+	// constructors
+	Areas() {
+		
+	}
+	Areas(int roomNum) {
+		int area = roomNum;
+		// check if hey have been to the room before
+		// if not, determine if this room has an item and set enemies
+		if (!beenTo[area]) {
+			determineIfHasItem(area);
+			generateEnemies(area);
+			setBeenTo(area);
+		}
+		// If the room has an item, check if the player has gotten the item. If not, set the item.
+		if ((hasItem[area]) && (!gottenItem[area])) {
+			setItemName("itemNameHere"); // get item name from array of item names
+		}
+	}
     
     // getters
     public static int getArea() {
@@ -38,6 +43,9 @@ public class Areas {
     }
     public static int getLastArea() {
         return lastArea;
+    }
+    public static String getItemName() {
+    	return itemName;
     }
     
     //setters
@@ -47,193 +55,285 @@ public class Areas {
     public static void setLastArea(int newArea) {
         lastArea = newArea;
     }
+    public static void setBeenTo(int roomNum) {
+    	beenTo[roomNum] = true;
+    }
+    public static void setHasItem(int roomNum) {
+    	hasItem[roomNum] = true;
+    }
+    public static void setGottenItem(int roomNum) {
+    	gottenItem[roomNum] = true;
+    }
+    private static void setItemName(String item) {
+    	itemName = item;
+    }
     
     
-    // Display image
-    // Determine if the player has been to the area before. 
-    // If not, display the first-time visit message.
+    // If its the first time to that room:
+    	// Create new Areas object for the room and display the first-time-to-room message
+    	// Display first time area message
+    // If not the first time to the room, set room image and message
     public static void activateArea() {
+		SemesterProject.mapIV.setImage(roomPic);
     	switch (getArea()) {
-    		case 0:
-    	    	SemesterProject.mapIV.setImage(room0);
-    			// game start message    		
-				SemesterProject.storyOutputTF.appendText("\nYou see a cave entrance hidden behind some trees "
-						+ "and brush. The stench emanating from the entrance is a sure sign that it is "
-						+ "infested with goblins. Goblins always collect great treasures, but they never "
-						+ "travel alone. There is sure to be tougher things than goblins within the cave. "
-						+ "\nGo North to proceed.\n");
+    		case 0: 
+    			if (!beenTo[0]) {
+        			roomsArray[0] = new Areas(0);
+        			// game start message
+    				SemesterProject.storyOutputTF.appendText("\nYou see a cave entrance hidden behind some trees "
+    						+ "and brush. The stench emanating from the entrance is a sure sign that it is "
+    						+ "infested with goblins. Goblins always collect great treasures, but they never "
+    						+ "travel alone. There is sure to be tougher things than goblins within the cave. "
+    						+ "\nGo North to proceed.\n");
+    			}    			
     			break;
     		case 1:
-    			if (beenTo1) {
-                    // area message
-    				SemesterProject.storyOutputTF.appendText("\nArea 1\n");
-    			} else {
+    			if (!beenTo[1]) {
+    				roomsArray[1] = new Areas(1);
     				// first time area message
     				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 1\n");
-    				beenTo1 = true;
-                }
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 1\n");
                 break;
             case 2:              
-                if (beenTo2) {
-                    // area message
-                    SemesterProject.storyOutputTF.appendText("\nArea 2\n");
-                } else {
-                    // first time area message
-                    SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 2\n");
-                    beenTo2 = true;
-                }
+            	if (!beenTo[2]) {
+            		roomsArray[2] = new Areas(2);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 2\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 2\n");
                 break;
             case 3:               
-                if (beenTo3) {
-                    // area message
-                    SemesterProject.storyOutputTF.appendText("\nArea 3\n");
-                } else {
-                    // first time area message
-                    SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 3\n");
-                    beenTo3 = true;
-                }
+            	if (!beenTo[3]) {
+            		roomsArray[3] = new Areas(3);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 3\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 3\n");
                 break;
             case 4:
-            	SemesterProject.mapIV.setImage(room4);
-                if (beenTo4) {
-                    // area message
-                    SemesterProject.storyOutputTF.appendText("\nArea 4\n");
-                } else {
-                    // first time area message
-                    SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 4\n");
-                    beenTo4 = true;
-                }
+            	if (!beenTo[4]) {
+            		roomsArray[4] = new Areas(4);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 4\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 4\n");
                 break;
             case 5:
-                if (beenTo5) {
-                    // area message
-                    SemesterProject.storyOutputTF.appendText("\nArea 5\n");
-                } else {
-                    // first time area message
-                	SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 5\n");
-                    beenTo5 = true;
-                }
+            	if (!beenTo[5]) {
+            		roomsArray[5] = new Areas(5);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 5\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 5\n");
                 break;
             case 6:                
-                if (beenTo6) {
-                    // area message
-                    SemesterProject.storyOutputTF.appendText("\nArea 6\n");
-                } else {
-                    // first time area message
-                    SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 6\n");
-                    beenTo6 = true;
-                }
+            	if (!beenTo[6]) {
+            		roomsArray[6] = new Areas(6);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 6\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 6\n");
                 break;
             case 7:               
-               	if (beenTo7) {
-                    // area message
-                    SemesterProject.storyOutputTF.appendText("\nArea 7\n");
-                } else {
-                    // first time area message
-                    SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 7\n");
-                    beenTo7 = true;
-                }
+            	if (!beenTo[7]) {
+            		roomsArray[7] = new Areas(7);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 7\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 7\n");
                 break;
             case 8:               
-                if (beenTo8) {
-                    // area message
-                    SemesterProject.storyOutputTF.appendText("\nArea 8\n");
-                } else {
-                    // first time area message
-                    SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 8\n");
-                    beenTo8 = true;
-                }
+            	if (!beenTo[8]) {
+            		roomsArray[8] = new Areas(8);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 8\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 8\n");
                 break;
             case 9:                
-                if (beenTo9) {
-                    // area message
-                    SemesterProject.storyOutputTF.appendText("\nArea 9\n");
-                } else {
-                    // first time area message
-                    SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 9\n");
-                    beenTo9 = true;
-                }
+            	if (!beenTo[9]) {
+            		roomsArray[9] = new Areas(9);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 9\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 9\n");
                 break;
             case 10:               
-                if (beenTo10) {
-                    // area message
-                    SemesterProject.storyOutputTF.appendText("\nArea 10\n");
-                } else {
-                    // first time area message
-                    SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 10\n");
-                    beenTo10 = true;
-                }
+            	if (!beenTo[10]) {
+            		roomsArray[10] = new Areas(10);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 10\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 10\n");
                 break;
             case 11:
-                if (beenTo11) {
-                	// area message
-                    SemesterProject.storyOutputTF.appendText("\nArea 11\n");
-                } else {
-                    // first time area message
-                	SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 11\n");
-                    beenTo11 = true;
-                }
+            	if (!beenTo[11]) {
+            		roomsArray[11] = new Areas(11);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 11\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 11\n");
                 break;
             case 12:
-                if (beenTo12) {
-                	// area message
-                	SemesterProject.storyOutputTF.appendText("\nArea 12\n");
-                } else {
-                	// first time area message
-                	SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 12\n");
-                	beenTo12 = true;
-                }
+            	if (!beenTo[12]) {
+            		roomsArray[12] = new Areas(12);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 12\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 12\n");
                 break;
             case 13:
-                if (beenTo13) {
-                	// area message
-                	SemesterProject.storyOutputTF.appendText("\nArea 13\n");
-                } else {
-                	// first time area message
-                	SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 13\n");
-                	beenTo13 = true;
-                }
+            	if (!beenTo[13]) {
+            		roomsArray[13] = new Areas(13);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 13\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 13\n");
                 break;
             case 14:
-                if (beenTo14) {
-                	// area message
-                	SemesterProject.storyOutputTF.appendText("\nArea 14\n");
-                } else {
-                	// first time area message
-                	SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 14\n");
-                	beenTo14 = true;
-                }
+            	if (!beenTo[14]) {
+            		roomsArray[14] = new Areas(14);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 14\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 14\n");
                 break;
             case 15:
-                if (beenTo15) {
-                	// area message
-                	SemesterProject.storyOutputTF.appendText("\nArea 15\n");
-                } else {
-                	// first time area message
-                	SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 15\n");
-                	beenTo15 = true;
-                }
+            	if (!beenTo[15]) {
+            		roomsArray[15] = new Areas(15);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 15\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 15\n");
                 break;
             case 16:
-                if (beenTo16) {
-                	// area message
-                	SemesterProject.storyOutputTF.appendText("\nArea 16\n");
-                } else {
-                	// first time area message
-                	SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 16\n");
-                	beenTo16 = true;
-                }
+            	if (!beenTo[16]) {
+            		roomsArray[16] = new Areas(16);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 16\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 16\n");
                 break;
             case 17:
-                if (beenTo17) {
-                	// area message
-                	SemesterProject.storyOutputTF.appendText("\nArea 17\n");
-                } else {
-                	// first time area message
-                	SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 17\n");
-                	beenTo17 = true;
-                }
+                if (!beenTo[17]) {
+                	roomsArray[17] = new Areas(17);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 17\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 17\n");
+                break;
+            case 18:
+                if (!beenTo[18]) {
+                	roomsArray[18] = new Areas(18);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 18\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 18\n");
+                break;
+            case 19:
+                if (!beenTo[19]) {
+                	roomsArray[19] = new Areas(19);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 19\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 19\n");
+                break;
+            case 20:
+                if (!beenTo[20]) {
+                	roomsArray[20] = new Areas(20);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 20\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 20\n");
+                break;
+            case 21:
+                if (!beenTo[21]) {
+                	roomsArray[21] = new Areas(21);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 21\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 21\n");
+                break;
+            case 22:
+                if (!beenTo[22]) {
+                	roomsArray[22] = new Areas(22);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 22\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 22\n");
+                break;
+            case 23:
+                if (!beenTo[23]) {
+                	roomsArray[23] = new Areas(23);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 23\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 23\n");
+                break;
+            case 24:
+                if (!beenTo[24]) {
+                	roomsArray[24] = new Areas(24);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 24\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 24\n");
+                break;
+            case 25:
+                if (!beenTo[25]) {
+                	roomsArray[25] = new Areas(25);
+    				// first time area message
+    				SemesterProject.storyOutputTF.appendText("\nFirst Time to Area 25\n");
+    			}
+    			// area message
+    			SemesterProject.storyOutputTF.appendText("\nArea 25\n");
                 break;
             }
     }
+    
+    
+    private void determineIfHasItem(int roomNum) {
+    	if (getArea() != 0) {
+    		int percentChance = (int) Math.ceil(Math.random()*100);
+        	if (percentChance > 20) {
+        		setHasItem(roomNum);
+        	}
+    	}
+    }
+    
+    public static void generateEnemies(int roomNum) {
+    	// code to set the enemies for the specific room -------------------------------------------------------
+    }
+    
+    private void pickUpItem(int roomNum) {
+    	String item = getItemName();
+    	// code to set the stats for the specific item ---------------------------------------------------------
+    	
+    	setGottenItem(roomNum);
+    }
+    
 }
